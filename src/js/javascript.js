e20c21f5028bef3c90db348b85d6d5d343b09913
Ian Pelow <ipelow@kent.edu>
2018-07-09 00:32:30 -0400

    cleaning code

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -17,13 +17,10 @@ var popup = document.getElementById('myModal');
 
 // DROPDOWN
 
-         /* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
 if (!event.target.matches('.dropbtn')) {
 
